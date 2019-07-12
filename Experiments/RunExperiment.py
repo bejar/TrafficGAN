1d4bc6e8274e7a33370222accf01d35f8de340c7
javier.bejar <bejar@cs.upc.edu>
2019-07-12 10:41:33 +0200

    Adding some paremeters to training

--- Experiments/RunExperiment.py
+++ Experiments/RunExperiment.py
@@ -40,7 +40,7 @@ if __name__ == '__main__':
     parser.add_argument('--idate', default='20161101', help='First day')
     parser.add_argument('--fdate', default='20161130', help='Final day')
     parser.add_argument('--epochs', default=100, type=int, help='Epochs to train')
-    parser.add_argument('--verbos', action='store_true', default=False, help='Verbose output')
+    parser.add_argument('--verbose', action='store_true', default=False, help='Verbose output')
 
     os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
