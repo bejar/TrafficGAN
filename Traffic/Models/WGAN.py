adaf525c9a7394c87039c3db9fd548f18306a47c
javier.bejar <bejar@cs.upc.edu>
2019-07-16 13:46:48 +0200

    Adding option to change size of the convolution kernels

--- Traffic/Models/WGAN.py
+++ Traffic/Models/WGAN.py
@@ -145,7 +145,7 @@ class WGAN:
         # Because we normalized training inputs to lie in the range [-1, 1],
         # the tanh function should be used for the output of the generator to ensure
         # its output also lies in this range.
-        model.add(Convolution2D(chann, (5, 5), padding='same', activation='tanh'))
+        model.add(Convolution2D(chann, (self.ckernel, self.ckernel), padding='same', activation='tanh'))
         return model
 
     def make_discriminator(self):
